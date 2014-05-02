2732441a44c47389dd245a0ab3c6d4d9a97440f0
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2014-05-02 13:00:30 -0400

    Converted htr_dc into function

---
+++ run_htr_dc.py
@@ -0,0 +1,10 @@
+execfile('htr_dc.py')
+
+htr_dc('PFDM202T', 56.5, 64, t_start='2001:001', t_stop='2010:100', name='FDM-2', event='2010:099:16:54:00.000')
+#for i in range(3,9):
+#    figure(i)
+#    xlim([DateTime('2010:080').plotdate, DateTime('2010:105').plotdate])
+
+#htr_dc('PFDM202T', 56, 72, tstart='2008:001', t_stop='2012:001', name='5133 Line', event='2010:099:16:54:00.000')
+
+
