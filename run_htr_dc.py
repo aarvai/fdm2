5e49b7bf8c7a5daed3e3404c1508a473b4c67bd1
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2014-05-15 12:42:23 -0400

    Added run options

--- run_htr_dc.py
+++ run_htr_dc.py
@@ -1,10 +1,13 @@
 execfile('htr_dc.py')
 
-htr_dc('PFDM202T', 56.5, 64, t_start='2001:001', t_stop='2010:100', name='FDM-2', event='2010:099:16:54:00.000')
+#1.  FDM-2 Heater
+#htr_dc('PFDM202T', 56.5, 64, t_start='2001:001', t_stop='2010:100', name='FDM-2', event='2010:099:16:54:00.000', plot_cycles=False)
 #for i in range(3,9):
 #    figure(i)
 #    xlim([DateTime('2010:080').plotdate, DateTime('2010:105').plotdate])
 
-#htr_dc('PFDM202T', 56, 72, tstart='2008:001', t_stop='2012:001', name='5133 Line', event='2010:099:16:54:00.000')
-
+#2.  5133 Line Heater
+htr_dc('PLINE08T', 56, 72, t_start='2001:001', t_stop='2014:100', name='5133 Line', event='2010:099:16:54:00.000', plot_cycles=False)
+#htr_dc('PLINE08T', 56, 72, t_start='2010:197', t_stop='2010:199', name='5133 Line', event='2010:099:16:54:00.000', plot_cycles=True)
+#htr_dc('PLINE08T', 56, 72, t_start='2010:001', t_stop='2011:001', name='5133 Line', event='2010:099:16:54:00.000', plot_cycles=False)
 
