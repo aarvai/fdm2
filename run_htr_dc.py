2fb78833569a408e124ac457c772fe68b68e4705
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2014-05-16 15:45:29 -0400

    Added 5105 line heater runs

--- run_htr_dc.py
+++ run_htr_dc.py
@@ -1,13 +1,18 @@
 execfile('htr_dc.py')
 
 #1.  FDM-2 Heater
-#htr_dc('PFDM202T', 56.5, 64, t_start='2001:001', t_stop='2010:100', name='FDM-2', event='2010:099:16:54:00.000', plot_cycles=False)
+#htr_dc('PFDM202T', 57, 64, t_start='2009:197', t_stop='2009:199', name='FDM-2', event='2010:099:16:54:00.000', plot_cycles=True) #troubleshooting
+#htr_dc('PFDM202T', 57, 64, t_start='2001:001', t_stop='2010:099', name='FDM-2', event='2010:099:16:54:00.000', plot_cycles=False)
 #for i in range(3,9):
 #    figure(i)
 #    xlim([DateTime('2010:080').plotdate, DateTime('2010:105').plotdate])
 
 #2.  5133 Line Heater
-htr_dc('PLINE08T', 56, 72, t_start='2001:001', t_stop='2014:100', name='5133 Line', event='2010:099:16:54:00.000', plot_cycles=False)
-#htr_dc('PLINE08T', 56, 72, t_start='2010:197', t_stop='2010:199', name='5133 Line', event='2010:099:16:54:00.000', plot_cycles=True)
-#htr_dc('PLINE08T', 56, 72, t_start='2010:001', t_stop='2011:001', name='5133 Line', event='2010:099:16:54:00.000', plot_cycles=False)
+#htr_dc('PLINE08T', 58, 71, t_start='2010:197', t_stop='2010:199', name='5133 Line', event='2010:099:16:54:00.000', plot_cycles=True)  #troubleshooting
+htr_dc('PLINE08T', 58, 71, t_start='2001:001', t_stop='2014:100', name='5133 Line', event='2010:099:16:54:00.000', plot_cycles=False)
+#htr_dc('PLINE08T', 58, 71, t_start='2010:001', t_stop='2011:001', name='5133 Line', event='2010:099:16:54:00.000', plot_cycles=False)
 
+#2.  5105 Line Heater
+#htr_dc('PLINE01T', 59, 67.2, t_start='2010:300', t_stop='2010:330', name='5105 Line', event='2010:099:16:54:00.000', plot_cycles=True, dur_lim=500*60)  #troubleshooting
+#htr_dc('PLINE01T', 59, 67.2, t_start='2001:001', t_stop='2014:100', name='5105 Line', event='2010:099:16:54:00.000', plot_cycles=False, dur_lim=70*60)
+#htr_dc('PLINE01T', 55.5, 67.2, t_start='2010:001', t_stop='2011:001', name='5105 Line', event='2010:099:16:54:00.000', plot_cycles=False, dur_lim=500*60)
